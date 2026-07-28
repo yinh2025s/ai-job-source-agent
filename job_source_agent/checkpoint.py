b4d842e24b23730df8ab0b36c55a60ee640be2fb
yinh <95472715+yinh2025s@users.noreply.github.com>
2026-07-29 05:09:49 +0800

    Revert "Bump adapter version to .282"
    This reverts commit 264747bfb9857f1fbacc31e7544a1804421240f5.

--- job_source_agent/checkpoint.py
+++ job_source_agent/checkpoint.py
@@ -10,7 +10,7 @@ from .source_posting import source_posting_fingerprint_payload
 
 
 CHECKPOINT_SCHEMA_VERSION = "1.8"
-ADAPTER_VERSION = "2026-07-29.282"
+ADAPTER_VERSION = "2026-07-29.281"
 
 FINGERPRINT_FIELDS = (
     "company_name",
