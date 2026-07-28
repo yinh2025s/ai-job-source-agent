264747bfb9857f1fbacc31e7544a1804421240f5
yinh <95472715+yinh2025s@users.noreply.github.com>
2026-07-29 04:49:44 +0800

    Bump adapter version to .282

--- job_source_agent/checkpoint.py
+++ job_source_agent/checkpoint.py
@@ -10,7 +10,7 @@ from .source_posting import source_posting_fingerprint_payload
 
 
 CHECKPOINT_SCHEMA_VERSION = "1.8"
-ADAPTER_VERSION = "2026-07-29.281"
+ADAPTER_VERSION = "2026-07-29.282"
 
 FINGERPRINT_FIELDS = (
     "company_name",
